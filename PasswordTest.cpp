a19e5f7d537f24fc18cb99f7f2518f4736c115e7
Paolo Gonzalez <paoloag133@gmail.com>
2025-04-23 11:09:42 -0700

    tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,34 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, three_a_one_b)
+{
+	Password my_password;
+	ASSERT_EQ(3, my_password.count_leading_characters("aaab"));
+}
+
+TEST(PasswordTest, one_a_one_b)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("ab"));
+}
+
+TEST(PasswordTest, no_characters)
+{
+	Password my_password;
+	ASSERT_EQ(0, my_password.count_leading_characters(""));
+}
+
+TEST(PasswordTest, b_sandwitched_by_a)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("aba"));
+}
+
+TEST(PasswordTest, a_b_quad_a)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("abaaaa"));
+}
+
